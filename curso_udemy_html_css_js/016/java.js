8089e3e9f2102ea3d4ecead1b4bb418fdbcd9687
HAROLD.GARCIA <garpic@hotmail.com>
2021-10-03 16:18:06 -0500

    java y bootstrap

--- curso_udemy_html_css_js/016/java.js
+++ curso_udemy_html_css_js/016/java.js
@@ -1,1 +1,16 @@
 console.log("Hola");
+//document.getElementById ("cabecera").innerHTML="Cal"
+
+function sumar(){
+    const forma=document.getElementById("forma");
+    let numero1=forma["numero1"];
+    let numero2=forma["numero2"];
+    let resultado= parseInt(numero1.value) + parseInt(numero2.value);
+    if (isNaN(resultado)){
+        document.getElementById("resultado").innerHTML="No hay numeros";
+    }else{
+        document.getElementById("resultado").innerHTML="Resultado: " + resultado;
+    }
+
+    
+}
